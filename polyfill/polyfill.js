9a24aa25f72c2f2ca232bf813e3bdd99920bc9a5
Paul m. p. P <mail.peny@free.fr>
2020-09-18 17:09:48 +0200

    support for wasi_snapshot_preview1 of wasi sdk 11
    hi, btw where is the source code of polyfill.wasm ?

--- polyfill/polyfill.js
+++ polyfill/polyfill.js
@@ -2191,7 +2191,7 @@ Module['asm'] = function(global, env, providedBuffer) {
 
 // === Body ===
 
-var ASM_CONSTS = [function() { const imports = { wasi_unstable: WASIPolyfill };         let file = document.getElementById('input').files[0];         let file_with_mime_type = file.slice(0, file.size, 'application/wasm');         let response = new Response(file_with_mime_type);         wasi_instantiateStreaming(response, imports)         .then(obj => {             setInstance(obj.instance);             try {                 obj.instance.exports._start();             } catch (e) {                 if (e instanceof WASIExit) {                     handleWASIExit(e);                 } else {                 }             }         })         .catch(error => {             console.log('error! ' + error);         }); }];
+var ASM_CONSTS = [function() { const imports = { wasi_unstable: WASIPolyfill,  wasi_snapshot_preview1 : WASIPolyfill };         let file = document.getElementById('input').files[0];         let file_with_mime_type = file.slice(0, file.size, 'application/wasm');         let response = new Response(file_with_mime_type);         wasi_instantiateStreaming(response, imports)         .then(obj => {             setInstance(obj.instance);             try {                 obj.instance.exports._start();             } catch (e) {                 if (e instanceof WASIExit) {                     handleWASIExit(e);                 } else {                 }             }         })         .catch(error => {             console.log('error! ' + error);         }); }];
 
 function _emscripten_asm_const_i(code) {
   return ASM_CONSTS[code]();
