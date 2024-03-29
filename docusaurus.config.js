f8d068faad4dfe5eeb81374f052da0a386c775f7
Eric Gregory <eric@cosmonic.com>
2024-03-29 14:24:07 -0400

    Align config with build
    Signed-off-by: Eric Gregory <eric@cosmonic.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://bytecodealliance.github.io',
+  url: 'https://wasi.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/wasi.dev/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
